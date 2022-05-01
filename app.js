3b696190596faffb9e33576d79bd7220684a13d6
surasit-su-mea <surasit.su@mea.or.th>
2022-05-01 08:20:28 +0700

    Add One Page

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const chalk = require('chalk')
-const debug = require('debug')
-const morgan = require('morgan')
+const chalk = require('chalk');
+const debug = require('debug');
+const morgan = require('morgan');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/",(req,res) =>{
 
